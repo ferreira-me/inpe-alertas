d5196a43daea4b071dd94120fd777e36be0a6eff
Maria Eduarda Ferreira <162278875+ferreira-me@users.noreply.github.com>
2025-09-25 21:06:51 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2022: true, node: true },
+  env: { browser: true, es2022: true, node: true, jest: true },
   extends: ['eslint:recommended'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   rules: {}
